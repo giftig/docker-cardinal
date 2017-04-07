558557d9b117bfe984fde297dce0358aeb10bc96
Rob Moore <rob.moore@adstream.com>
2017-04-07 17:44:05 +0100

    Pass $TERM through to docker

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$1" == '' ]]; then
-  cat /dev/stdin | docker run -i --rm giftig/cardinal:latest
+  cat /dev/stdin | docker run -i -e TERM --rm giftig/cardinal:latest
 else
   docker run -i --rm giftig/cardinal:latest "$@"
 fi
